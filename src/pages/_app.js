051bd8d6696d28548a3e8dd279aca6692b7e9af3
Evgeny Bobkin <evgeny.bobkin@europace.de>
2022-04-23 17:32:30 +0200

    include global style inside of theme provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,10 +7,14 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family:  ${({ theme }) => theme.font.family};
   }
 `
 
 const theme = {
+  font: {
+    family: "Montserrat, 'Open Sans', sans-serif",
+  },
   colors: {
     primary: '#0070f3',
   },
@@ -18,12 +22,10 @@ const theme = {
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
 
