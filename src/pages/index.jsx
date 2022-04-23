5f5c787094b945406d6899550add08dea535d011
Evgeny Bobkin <evgeny.bobkin@europace.de>
2022-04-23 17:15:18 +0200

    initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Memory game" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>memory</div>
+    </>
+  )
+}
